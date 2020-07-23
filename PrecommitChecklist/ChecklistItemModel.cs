7984755bf394adbc6dd45c948dd82b6f96405ede
LucaBrasi89 <andrew.sotnikov@zoho.com>
2020-07-23 23:41:36 +0300

    Initial commit

---
+++ PrecommitChecklist/ChecklistItemModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecommitChecklist
+{
+    public class ChecklistItemModel
+    {
+        string question;
+        bool answer;
+
+        public string Question { get => question; set => question = value; }
+        public bool Answer { get => answer; set => answer = value; }
+
+        public ChecklistItemModel(string question, bool answer)
+        {
+            this.question = question;
+            this.answer = answer;
+        }
+    }
+}
---
+++ PrecommitChecklist/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecommitChecklist
+{
+    public class MainWindowViewModel
+    {
+         public List<ChecklistItemModel> ChecklistItems
+        {
+            get
+            {
+                return new List<ChecklistItemModel>
+                {
+                    new ChecklistItemModel("Does all project in solution compile?", true),
+                    new ChecklistItemModel("Does UI run?", true),
+                    new ChecklistItemModel("Does App work correct?", true),
+                    new ChecklistItemModel("Have you double checked that only relevant files were added?", true),
+                    new ChecklistItemModel("Does commit comments include TFS task ID?", true),
+                    new ChecklistItemModel("Have you added \"Build X.Y.Z\" text in commit in the case" +
+                    "of exteranl release?", true),
+                    new ChecklistItemModel("Does header have right version number?", true)
+                };
+            }
+        }
+    }
+
+
+
+}
